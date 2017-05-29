a1cf00fec0d4cbde8b3f4150fb9e0d36f31c5c3e
Arlen Neylon <arlen.m.neylon@gmail.com>
2017-05-28 20:23:28 -0600

    basic react setup

--- server/index.js
+++ server/index.js
@@ -1,10 +1,14 @@
 const path = require('path')
 const express = require('express')
 const app = express()
-
+const morgan = require('morgan')
 const port = process.env.PORT || 8080
 
 app.use(express.static('public'))
+
+if(process.env.MODE === 'dev'){
+  app.use(morgan('dev'))
+}
 
 app.get('*', (req, res) => {
   // res.sendFile(path.resolve(__dirname, '../public/index.html'))
---
+++ src/js/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './components/app'
+
+// const App = () => (
+//   <div>Hello React</div>
+// )
+
+ReactDOM.render(<App message="test" />, document.querySelector('#app'))
