f400a14e00a5d208e573ca2cb9214315d0ddb8c2
Arlen Neylon <arlen.m.neylon@gmail.com>
2017-05-29 21:23:51 -0600

    begin lastNumbers game

--- src/js/components/lastNumbers.jsx
+++ src/js/components/lastNumbers.jsx
@@ -1,11 +1,45 @@
 import React from 'react'
 
-const LastNumbers = () => (
-  <div className="w3-display-container game-container">
-    <div className="w3-display-middle">
-      <h1>Last Numbers</h1>
+const LastNumbers = () => {
+  let status = 'waiting'
+
+  function checkAnswer (e) {
+    console.log('checking answer')
+    e.preventDefault()
+  }
+
+  function startStop() {
+    let startStopButton = document.getElementById('startStopButton')
+
+    if(status === 'waiting') {
+      status = 'starting'
+      startStopButton.innerHTML = 'Stop'
+    } else {
+      startStopButton.innerHTML = 'Start'
+      status = 'waiting'
+    }
+  }
+
+  return (
+    <div className="w3-display-container game-container">
+      <div className="w3-display-middle w3-card-2 w3-white w3-padding-16">
+        <h1>Last Numbers</h1>
+        <hr />
+        <h2 className="w3-light-grey">Click Start</h2>
+        <form onSubmit={checkAnswer}>
+          <input
+            className="w3-input w3-center"
+            type="number"
+            placeholder="###"
+            />
+        </form>
+        <button
+          className="w3-btn w3-blue"
+          onClick={startStop}
+          id="startStopButton">Start</button>
+      </div>
     </div>
-  </div>
-)
+  )
+}
 
 export default LastNumbers
