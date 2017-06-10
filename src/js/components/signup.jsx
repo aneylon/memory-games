c8b4b2ddc5dd786189e3d0a07bf4efe89e00e31d
Arlen Neylon <arlen.m.neylon@gmail.com>
2017-06-09 22:02:01 -0600

    add signin / signup components

---
+++ src/js/components/signup.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const SignUp = () => (
+  <div className='w3-display-container game-container'>
+    <div className='w3-display-middle w3-card-2 w3-white w3-padding-16'>
+      <h1>
+        SignUp
+      </h1>
+      <form>
+        <input type='text' placeholder='email'/>
+        <input type='password' placeholder='password'/>
+        <input type='password' placeholder='password'/>
+        <button>Sign Up</button>
+      </form>
+    </div>
+  </div>
+)
+
+export default SignUp
