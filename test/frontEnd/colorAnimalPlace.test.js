df2ec9da609f00845a72a4f1a5e8099d4f43bed9
Arlen Neylon <arlen.m.neylon@gmail.com>
2017-06-16 13:09:27 -0600

    update tests

--- test/frontEnd/colorAnimalPlace.test.js
+++ test/frontEnd/colorAnimalPlace.test.js
@@ -4,7 +4,8 @@ import { mount, shallow, render } from 'enzyme'
 import ColorAnimalPlace from '../../src/js/components/colorAnimalPlace'
 
 describe('<ColorAnimalPlace />', () => {
-  let colorAnimalPlace = shallow(<ColorAnimalPlace />)
+  // let colorAnimalPlace = shallow(<ColorAnimalPlace />)
+  let colorAnimalPlace = mount(<ColorAnimalPlace />)
 
   it('should render', () => {
     expect(colorAnimalPlace.find('h1').text()).to.equal('Color Animal Place')
