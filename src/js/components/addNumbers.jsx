423087803fd2484087e40c1379755d08d7439704
Arlen Neylon <arlen.m.neylon@gmail.com>
2017-06-18 10:34:50 -0600

    add base and test for new games, update readme.

---
+++ src/js/components/addNumbers.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import GameComponent from './gameComponent'
+
+const AddNumbers = () => (
+  <GameComponent
+    title="Add Numbers"
+    />
+)
+
+export default AddNumbers
